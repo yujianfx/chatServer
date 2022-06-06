cd8586605b4da6ecbd3c187d54f4b878d24607bc
26530 <2653084650@qq.com>
2022-06-06 21:40:59 +0800

    修改了打包命令

--- src/test/java/top/wangudiercai/chatserver/ChatServerApplicationTests.java
+++ src/test/java/top/wangudiercai/chatserver/ChatServerApplicationTests.java
@@ -3,6 +3,7 @@ package top.wangudiercai.chatserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 class ChatServerApplicationTests {
